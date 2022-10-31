743b954c51a7803c97dec018cd5cd196e72e3368
SATHIYAMOORTHY007 <67869734+SATHIYAMOORTHY007@users.noreply.github.com>
2022-10-31 09:42:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,129 @@
+function hallbackhell()
+{
+        setTimeout(()=>
+        {
+                let div= document.createElement("div");
+                document.body.append(div);
+                div.setAttribute("id","div");
+                let num = document.createElement("span");
+                num.setAttribute("id","span")
+                    num.innerHTML=9;
+                div.append(num);
+
+                setTimeout(()=>
+                {
+                        let del=document.getElementById("div")
+                        del.innerHTML = "";
+                       
+                        div.setAttribute("id","div");
+                                let num = document.createElement("span");
+                                num.setAttribute("id","span")
+                                num.innerHTML=8;
+                                div.append(num);
+                        setTimeout(()=>
+                        {
+                                let del=document.getElementById("div")
+                                del.innerHTML = "";
+                               
+                                div.setAttribute("id","div");
+                                let num = document.createElement("span");
+                                num.setAttribute("id","span")
+                                num.innerHTML=7;
+                                div.append(num);
+                                setTimeout(()=>
+                                {
+                                        let del=document.getElementById("div")
+                                        del.innerHTML = "";
+                                       
+                                        div.setAttribute("id","div");
+                                        let num = document.createElement("span");
+                                        num.setAttribute("id","span")
+                                        num.innerHTML=6;
+                                        div.append(num);
+
+                                         setTimeout(()=>
+                                        {
+                                                let del=document.getElementById("div")
+                                                del.innerHTML = "";
+                                        
+                                                div.setAttribute("id","div");
+                                                let num = document.createElement("span");
+                                                num.setAttribute("id","span")
+                                                num.innerHTML=5;
+                                                div.append(num);
+                                              
+                                                 setTimeout(()=>
+                                                 {
+                                                        let del=document.getElementById("div")
+                                                        del.innerHTML = "";
+                                                       
+                                                        div.setAttribute("id","div");
+                                                        let num = document.createElement("span");
+                                                        num.setAttribute("id","span")
+                                                        num.innerHTML=4;
+                                                        div.append(num);
+                                                          setTimeout(()=>
+                                                          {
+                                                                let del=document.getElementById("div")
+                                                                        del.innerHTML = "";
+                                                                
+                                                                        div.setAttribute("id","div");
+                                                                        let num = document.createElement("span");
+                                                                        num.setAttribute("id","span")
+                                                                        num.innerHTML=3;
+                                                                        div.append(num);
+                                                                   setTimeout(()=>
+                                                                   {
+                                                                        let del=document.getElementById("div")
+                                                                        del.innerHTML = "";
+                                                                       
+                                                                        div.setAttribute("id","div");
+                                                                        let num = document.createElement("span");
+                                                                        num.setAttribute("id","span")
+                                                                        num.innerHTML=2;
+                                                                        div.append(num);
+                                                                            setTimeout(()=>
+                                                                            {
+                                                                                let del=document.getElementById("div")
+                                                                                del.innerHTML = "";
+                                                                               
+                                                                                div.setAttribute("id","div");
+                                                                                let num = document.createElement("span");
+                                                                                num.setAttribute("id","span")
+                                                                                num.innerHTML=1;
+                                                                                div.append(num);
+                                                                                        setTimeout(()=>
+                                                                                        {
+                                                                                                let del=document.getElementById("div")
+                                                                                                del.innerHTML = "";
+                                                                                               
+                                                                                                div.setAttribute("id","div");
+                                                                                                let num = document.createElement("span");
+                                                                                                num.setAttribute("id","span1")
+                                                                                                num.innerHTML="Happy";
+                                                                                                div.append(num);
+
+                                                                                                let num1= document.createElement("span");
+                                                                                                num1.setAttribute("id","span2")
+                                                                                                num1.innerHTML=" independence";
+                                                                                                div.append(num1);
+
+                                                                                                let num2 = document.createElement("span1");
+                                                                                                num2.setAttribute("id","span3")
+                                                                                                num2.innerHTML=" day";
+                                                                                                div.append(num2);
+                                                                                        },2000);
+                                                                            },2000);
+                                                                   },2000);
+                                                          },2000);
+                                                 },2000);
+                                        },2000);
+                                },2000);
+                        },2000);
+                },2000);
+               
+                
+                
+        },2000);
+}
+hallbackhell()
